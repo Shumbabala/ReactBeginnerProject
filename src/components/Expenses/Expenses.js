0c8eeee64d09cb4b5b0c01901cb85b55b40fcef7
Shumbabala <notoriousreking@gmail.com>
2023-07-26 10:50:28 +0200

    first commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,35 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  /*return statement has same structure as in App component, 
+  except you need to extract each of the ExpenseItem components off of the
+  'props' array submitted into this component as a parameter*/
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
