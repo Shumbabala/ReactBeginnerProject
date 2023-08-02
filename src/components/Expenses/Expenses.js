49d7faa566f363592cbd9cf2cae7834e7a8c0c95
Shumbabala <notoriousreking@gmail.com>
2023-08-02 17:37:55 +0200

    react app not loading, send to github

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,7 +12,7 @@ function Expenses(props) {
   function FilterChangeHandler(event) {
     setChosenFilter(event.target.value);
   }
- 
+
   //***
 
   /*return statement has same structure as in App component, 
@@ -24,27 +24,13 @@ function Expenses(props) {
         selectedFilter={chosenFilter}
         onFilterChange={FilterChangeHandler}
       ></ExpenseFilter>
-      //git fourth commit
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      ></ExpenseItem>
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.data}
+        /> 
+      ))}
     </Card>
   );
 }
