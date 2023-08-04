c238a7c1b56ce3de35614803c99042fca50b8d2a
Shumbabala <notoriousreking@gmail.com>
2023-08-04 12:15:54 +0200

    updating Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,21 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "../NewExpense/ExpenseFilter";
+import ExpenseItem from "./ExpenseItem";
 import { useState } from "react";
 
 function Expenses(props) {
   //we need to declare a state where we'll store the filter info from ExpenseFilter component
-  const [chosenFilter, setChosenFilter] = useState("Date");
+  const [chosenFilter, setChosenFilter] = useState("2022");
+  //using state to represent the filtered expenses
   const [expensesArray, setExpensesArray] = useState(props.expenses);
 
   function filterChangeHandler(event) {
     setChosenFilter(event.target.value);
-    //warning: all these console.logs are only for testing purpouses, not to be in definitive version
-    console.log(
-      props.expenses.filter(
-        (expense) => expense.date.getFullYear() === parseInt(chosenFilter)
-      )
-    );
-    console.log("Expenses value: " + event.target.value);
-    console.log("chosenFilter value: " + parseInt(chosenFilter));
+
     /*Assignment 3 code: output correct ExpenseItem list depending on'chosenFilter' parameter*/
+    //this code seems to be the main issue of the problem,
+    //giving "TypeError: Cannot read properties of undefined (reading 'filter')" on React App
     setExpensesArray((props) => {
       props.expenses.filter((expense) => {
         return expense.date.getFullYear() === parseInt(chosenFilter);
@@ -27,23 +23,17 @@ function Expenses(props) {
     });
   }
 
-  /*return statement has same structure as in App component, 
-  except you need to extract each of the ExpenseItem components off of the
-  'props' array submitted into this component as a parameter*/
   return (
-    <Card className="expenses">
-      <ExpenseFilter
-        selectedFilter={chosenFilter}
-        onFilterChange={filterChangeHandler}
-      ></ExpenseFilter>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selectedFilter={chosenFilter}
+          onFilterChange={filterChangeHandler}
+        ></ExpenseFilter>
 
-      {/*the code below works, but it's not part of the code version I'm trying to make work, can ignore*/}
-      {/*}
-      {props.expenses
-        .filter(
-          (expense) => expense.date.getFullYear() === parseInt(chosenFilter)
-        )
-        .map((expense) => (
+        {/*this is the rendering code that fails to work*/}
+
+        {expensesArray.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
@@ -51,18 +41,8 @@ function Expenses(props) {
             date={expense.date}
           />
         ))}
-        */}
-
-      {/*this is the rendering code that fails to work*/}
-      {expensesArray.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+      </Card>
+    </div>
   );
 }
 
